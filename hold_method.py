2c26ac7e296a90ba841ba25e30aa078647a32ef6
nvk23 <nkuznetsov23@gmail.com>
2023-07-18 22:14:15 -0400

    Updated Edge Case Selection to show more plots

--- hold_method.py
+++ hold_method.py
@@ -42,11 +42,11 @@ def plot_clusters(df, x_col='theta', y_col='r', gtype_col='gt', title='snp plot'
             # # fig.update_traces(opacity=opacity)   
 
             # hold_no_snp = df.drop(df[df['snpID'] == single_snp].index)
-            fig = fig = px.scatter(df, x=x_col, y=y_col, color=gtype_col, color_discrete_map = cmap, color_continuous_scale=px.colors.sequential.matter, width=650, height=497, labels=lmap, symbol='phenotype', symbol_map=smap, hover_data=['GT', 'R_tightness', 'Theta_tightness'])
+            fig = px.scatter(df, x=x_col, y=y_col, color=gtype_col, color_discrete_map = cmap, color_continuous_scale=px.colors.sequential.matter, width=650, height=497, labels=lmap, symbol='phenotype', symbol_map=smap, hover_data=['GT', gtype_col, 'R_tightness', 'Theta_tightness'])
             fig.update_traces(opacity = opacity)
-            fig.add_traces(px.scatter(df.loc[df['snpID'] == single_snp], x=x_col, y=y_col).update_traces(marker_color="black").data)
+            fig.add_traces(px.scatter(df.loc[df['snpID'] == single_snp], x=x_col, y=y_col, hover_data=['GT', gtype_col, 'R_tightness', 'Theta_tightness']).update_traces(marker_color="black").data)
         else:
-            fig = px.scatter(df, x=x_col, y=y_col, color=gtype_col, color_discrete_map = cmap, color_continuous_scale=px.colors.sequential.matter, width=650, height=497, labels=lmap, symbol='phenotype', symbol_map=smap, hover_data=['GT', 'R_tightness', 'Theta_tightness'])
+            fig = px.scatter(df, x=x_col, y=y_col, color=gtype_col, color_discrete_map = cmap, color_continuous_scale=px.colors.sequential.matter, width=650, height=497, labels=lmap, symbol='phenotype', symbol_map=smap, hover_data=['GT', gtype_col, 'R_tightness', 'Theta_tightness'])
 
     else:
         fig = px.scatter(df, x=x_col, y=y_col, color=gtype_col, color_discrete_map=cmap, width=650, height=497, labels=lmap, symbol='phenotype', symbol_map=smap)
--- pages/Edge_Case_Selection.py
+++ pages/Edge_Case_Selection.py
@@ -18,58 +18,96 @@ from hold_method import plot_clusters
 
 st.title('Interactive Previous No Calls Evaluation')
 
+def find_unmatched(df_all):
+    df_all = df_all.reset_index()
+    mask = (df_all['GT'] != 'NC') & (df_all['preds_cat'] != 'NC')
+
+    # Check if 'GT' is equal to 'preds' where the mask is True
+    result = (df_all['GT'] == df_all['preds_cat'])[mask]
+    df_all['not_NC_check'] = result
+    unmatched = df_all.loc[df_all['not_NC_check'] == False, 'snpID']
+    # display(df_all[['GT', 'R', 'preds_cat', 'not_NC_check']].loc[df_all['not_NC_check'] == False])
+
+    return unmatched.values
+
+def display_plots(snp_name, full_metrics, full_plot = True, before_after = True, title = None): 
+    snp1 = full_metrics.loc[full_metrics['snpID'] == snp_name]
+
+    if before_after:
+        cluster_metric_before = plot_clusters(snp1, x_col='Theta', y_col='R', gtype_col='GT', title = "Before Recluster")['fig']
+        st.plotly_chart(cluster_metric_before, use_container_width = True)
+
+        cluster_metric_after = plot_clusters(snp1, x_col='Theta', y_col='R', gtype_col='preds_cat', title = "After Recluster")['fig']
+        st.plotly_chart(cluster_metric_after, use_container_width=True)
+    else:
+        cluster_metric = plot_clusters(snp1, x_col='Theta', y_col='R', gtype_col='preds_cat', title = title)['fig']
+        st.plotly_chart(cluster_metric, use_container_width = True)
+
+    if full_plot:
+        cluster_full = plot_clusters(full_metrics, x_col='Theta', y_col='R', gtype_col='preds_cat', title = "Full Plot", single_snp = snp_name, opacity = 0.2)['fig']
+        st.plotly_chart(cluster_full, use_container_width=True)
+
+
 if 'snp_choices' not in st.session_state:
     st.session_state['snp_choices'] = []
+if 'snp_name' not in st.session_state:
+    st.session_state['snp_name'] = ''
 
 full_metrics = pd.read_csv('data/060623_full_cluster_tightness_wip')
-tightness_measure = st.sidebar.selectbox(label = 'Choose the Outlier Metric', options=['R', 'Theta'])
-rand_generate = st.sidebar.checkbox('Randomly generate SNPs')
+before_matched = pd.read_csv('data/060623_complete')
 
-if tightness_measure == 'R':
-    measure = 'R_outlier'
-elif tightness_measure == 'Theta':
-    measure = 'Theta_outlier'
+display_unmatched = st.sidebar.checkbox('Display discrepancy SNPs')
 
-flagged_outliers = full_metrics[full_metrics[measure] == True]
-prev_nc = flagged_outliers[flagged_outliers["GT"] == "NC"]
-now_nc = flagged_outliers[flagged_outliers["preds_cat"] == "NC"]
+if not display_unmatched:
+    rand_generate = st.sidebar.checkbox('Randomly generate SNPs')
+    tightness_measure = st.sidebar.selectbox(label = 'Choose the Outlier Metric', options=['R', 'Theta'])
 
-exclude_snps = set(now_nc['snpID'].unique())
-include_snps = set(prev_nc['snpID'].unique())
-include = include_snps.difference(exclude_snps)
+    if tightness_measure == 'R':
+        measure = 'R_outlier'
+    elif tightness_measure == 'Theta':
+        measure = 'Theta_outlier'
 
-if not rand_generate:
-    st.sidebar.markdown('### Choose an outlier SNP to display')
-    snp_name = st.sidebar.selectbox(label = 'SNP Name Choice', label_visibility = 'collapsed', options=include)
+    flagged_outliers = full_metrics[full_metrics[measure] == True]
+    prev_nc = flagged_outliers[flagged_outliers["GT"] == "NC"]
+    now_nc = flagged_outliers[flagged_outliers["preds_cat"] == "NC"]
 
-    snp1 = full_metrics.loc[full_metrics['snpID'] == snp_name]
-else:
-    snp_name = random.choice(list(include))
-    snp1 = full_metrics.loc[full_metrics['snpID'] == snp_name]
+    exclude_snps = set(now_nc['snpID'].unique())
+    include_snps = set(prev_nc['snpID'].unique())
+    include = include_snps.difference(exclude_snps)
 
-    st.markdown(f'#### SNP: {snp_name}')
-    btn1, btn2, btn3 = st.columns([1, 0.5, 0.5])
-    btn1.markdown('#### Should this reclustering be regenerated?')
-    regenerate = btn2.button('Yes')
-    no_regenerate = btn3.button('No')
+    if not rand_generate:
+        st.sidebar.markdown('### Choose an outlier SNP to display')
+        snp_name = st.sidebar.selectbox(label = 'SNP Name Choice', label_visibility = 'collapsed', options=include)
 
-    if regenerate:
-        st.session_state['snp_choices'].append(snp_name)
-        snp_name = random.choice(list(include))
-        snp1 = full_metrics.loc[full_metrics['snpID'] == snp_name]
-    elif no_regenerate:
-        snp_name = random.choice(list(include))
-        snp1 = full_metrics.loc[full_metrics['snpID'] == snp_name] 
+        display_plots(snp_name, full_metrics)
 
-    st.sidebar.markdown('Reported SNPs:')
-    st.sidebar.write(st.session_state['snp_choices'])  # MAKE DOWNLOAD BUTTON FOR TXT FILE OF THESE SNPS
+    else:
+        st.session_state['snp_name'] = random.choice(list(include))
+        snp1 = full_metrics.loc[full_metrics['snpID'] == st.session_state['snp_name']]
 
-cluster_metric_before = plot_clusters(snp1, x_col='Theta', y_col='R', gtype_col='GT', title = "Before Recluster")['fig']
-st.plotly_chart(cluster_metric_before, use_container_width = True)
+        st.markdown(f'#### SNP: {st.session_state["snp_name"]}')
+        btn1, btn2, btn3 = st.columns([1, 0.5, 0.5])
+        btn1.markdown('#### Should this reclustering be regenerated?')
+        regenerate = btn2.button('Yes')
+        no_regenerate = btn3.button('No')
 
-cluster_metric_after = plot_clusters(snp1, x_col='Theta', y_col='R', gtype_col='preds_cat', title = "After Recluster")['fig']
-st.plotly_chart(cluster_metric_after, use_container_width=True)
+        if regenerate:
+            st.session_state['snp_choices'].append(st.session_state['snp_name'])
+            snp_name = random.choice(list(include))
+        elif no_regenerate:
+            snp_name = random.choice(list(include))
 
-cluster_full = plot_clusters(full_metrics, x_col='Theta', y_col='R', gtype_col='preds_cat', title = "Full Plot", single_snp = snp_name, opacity = 0.2)['fig']
-st.plotly_chart(cluster_full, use_container_width=True)
+        st.sidebar.markdown('Reported SNPs:')
+        st.sidebar.write(st.session_state['snp_choices'])  # MAKE DOWNLOAD BUTTON FOR TXT FILE OF THESE SNPS
+        display_plots(st.session_state['snp_name'], full_metrics)
+
+elif display_unmatched:
+    # Temporarily hard-coded for presentation purposes
+    discrepancies = ['Variant49196', 'rs188740886', 'chr4:89854340:T:A_ilmnfwd_ilmnF2BT', '6:161575165-TG', 'rs1475032', 'rs6913878']
+    # discrepancies = find_unmatched(before_matched)
+
+    snp_name = st.sidebar.selectbox(label = 'SNP Name Choice', label_visibility = 'collapsed', options=discrepancies)
+
+    display_plots(snp_name, before_matched, full_plot = False, before_after = False, title = 'Our Prediction')
+    display_plots(snp_name, full_metrics, before_after = False, title = "Illumina's Prediction")
 
