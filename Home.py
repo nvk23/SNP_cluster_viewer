d2b12f9de48cc4bd7592edf823704f9dce8ee851
Nicole Kuznetsov <61252050+nvk23@users.noreply.github.com>
2024-09-30 23:06:17 -0400

    Create side-by-side plot comparisons

--- Home.py
+++ Home.py
@@ -28,20 +28,34 @@ st.set_page_config(
 
 st.title('SNCA Artifact SNPs vs. Non-Artifact SNPs')
 
-artifact_snps = pd.read_csv(f'data/')
-non_artifact_snps = pd.read_csv(f'data/')
+artifact_snps = pd.read_csv(f'data/sample_info/artifact_SNCA_SNPs.csv')
+non_artifact_snps = pd.read_csv(f'data/sample_info/non_artifact_SNCA_SNPs.csv')
 
+
+st.markdown('*:red[Believed artifact range: 89800000-89870000]*')
 artifact,non_artifact = st.columns(2)
-
-# Gives user option to choose studies based on their names or target disease
-artifact_name = artifact.selectbox('Artifact SNP Selection:', options=artifact_snps.snpID.values)
-non_artifact_name = non_artifact.selectbox('Non-PAR SNP Selection:', options=non_artifact_snps.snpID.values)
 
 artifact.markdown('### Artifact SNPs')
 non_artifact.markdown('### Non-Artifact SNPs')
 
-artifact.metric(f'Number of available samples:', "{:.0f}".format(len(np.unique(nba_raw.IID))))
-non_artifact.metric(f'Number of available samples:', "{:.0f}".format(len(np.unique(nba_raw.IID))))
+# Gives user option to choose studies based on their names or target disease
+artifact_name = artifact.selectbox('Artifact SNP Selection:', options=artifact_snps.snpID.values)
+non_artifact_name = non_artifact.selectbox('Non-Artifact SNP Selection:', options=non_artifact_snps.snpID.values)
 
-art_plot = plot_clusters_seaborn(nba_raw, x_col='Theta', y_col='R', gtype_col='GT', title = f'{snp_name} NBA {nba_gt}', opacity = 1)['fig']
+art_snps = pd.read_csv(f'data/extracted_snps/{artifact_name}_main.csv')
+non_art_snps = pd.read_csv(f'data/extracted_snps/{non_artifact_name}_main.csv')
+
+artifact.metric(f'Number of available samples:', len(np.unique(art_snps.IID)))
+non_artifact.metric(f'Number of available samples:', len(np.unique(non_art_snps.IID)))
+
+artifact.metric(f'Position on chromosome:', artifact_snps[artifact_snps.snpID == artifact_name].iloc[0].position)
+non_artifact.metric(f'Position on chromosome:', non_artifact_snps[non_artifact_snps.snpID == non_artifact_name].iloc[0].position)
+
+art_plot = plot_clusters_seaborn(art_snps, x_col='Theta', y_col='R', gtype_col='GT', title = f'{artifact_name}', opacity = 1)['fig']
 artifact.pyplot(art_plot)
+
+non_art_plot = plot_clusters_seaborn(non_art_snps, x_col='Theta', y_col='R', gtype_col='GT', title = f'{non_artifact_name}', opacity = 1)['fig']
+non_artifact.pyplot(non_art_plot)
+
+artifact.dataframe(artifact_snps[artifact_snps.snpID == artifact_name])
+non_artifact.dataframe(non_artifact_snps[non_artifact_snps.snpID == non_artifact_name])
